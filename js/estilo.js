0cea7689f20ea320e8c67a847135ca2b45a60d35
Lucas Marcão <69424024+lucasmarcao@users.noreply.github.com>
2022-09-04 17:47:58 -0300

    SECAO-MARCAO-2: pizza design

--- js/estilo.js
+++ js/estilo.js
@@ -3,3 +3,13 @@
 
 
 // fim da navbar
+
+
+// cores foda
+
+// background-color: #2B181E;
+// background-gradient: #2B181E;
+// link-color: #f9c440;
+// link-hover-color: #d00;
+// text-color: #c5b184;
+// 3c212a fundo menores
